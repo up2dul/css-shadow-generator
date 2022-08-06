36b5ddf1ce956406c747804c833eb022fa667add
abdul <malik301002@gmail.com>
2022-08-06 15:51:53 +0700

    feat: add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Header } from './components';
 
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <Header />
+  );
 }
 
 export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <header className='p-10 text-center'>
+      <h1 className='text-3xl font-bold'>CSS Box Shadow Generator</h1>
+      <p>Easly generate box-shadow css code, with your own customization</p>
+    </header>
+  );
+}
