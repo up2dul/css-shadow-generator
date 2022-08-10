f10ee3a2037bd835cf87418279c88da6814e0afc
abdul <malik301002@gmail.com>
2022-08-10 07:04:41 +0700

    feat: finish the color-picker functionality

--- src/components/color-picker.jsx
+++ src/components/color-picker.jsx
@@ -1,17 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import { changeColor } from '@/features/box-shadow/box-shadow-slice';
 import { useDispatch, useSelector } from 'react-redux';
 import { RgbaStringColorPicker } from 'react-colorful';
+import { changeColor } from '@/features/box-shadow/box-shadow-slice';
 
 export const ColorPicker = () => {
-  const [color, setColor] = useState();
+  const { color: firstColor } = useSelector((state) => state.boxShadow);
+  const [color, setColor] = useState(firstColor);
+  const [isShowPicker, setIsShowPicker] = useState(false);
   const dispatch = useDispatch();
-  const box = useSelector((state) => state.boxShadow);
 
   useEffect(() => {
-    dispatch(changeColor({ newColor: color }));
-    console.log('dari colorpicker:', box);
+    dispatch(changeColor({ newColor: color }))
   }, [color]);
 
-  return <RgbaStringColorPicker color={color} onChange={setColor} />
+  const togglePicker = () => setIsShowPicker(!isShowPicker);
+
+  return (
+    <div className='mt-6 relative'>
+      <p>Shadow color</p>
+      <div 
+        className='p-2 w-24 border-solid border-4 border-custom-turquoise cursor-pointer'
+        style={{ backgroundColor: color }}
+        onClick={togglePicker}
+      ></div>
+      {isShowPicker && 
+        <div className='absolute'>
+          <RgbaStringColorPicker color={color} onChange={setColor} /> 
+        </div>
+      }
+    </div>
+  );
 }
